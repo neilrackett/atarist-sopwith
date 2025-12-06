neilrackett/atarist-sopwith

commit 7f8393dc12bdf611ad68a1d64953de2b87f6457e

Eliminate last uses of `ob_clr` for game logic
This field is now only ever used for decorative purposes, and we instead
use `ob_faction` for checking allegiance.

src/swcollsn.c
@@ -142,11 +142,13 @@ static OBJECTS *GetScoreObject(OBJECTS *ob, int *reverse)
 
 	if (playmode != PLAYMODE_ASYNCH) {
 		retval = planes[0];
-		if (ob->ob_clr == 1) {
+		if (ob->ob_faction == FACTION_PLAYER1) {
 			*reverse = 1;
 		}
 	} else {
-		retval = planes[2 - ob->ob_clr];
+		// TODO: Support more than two factions.
+		retval = planes[ob->ob_faction == FACTION_PLAYER1 ?
+		                FACTION_PLAYER2 : FACTION_PLAYER1];
 	}
 
 	return retval;

src/swmove.c
@@ -72,14 +72,16 @@ void swmove(void)
 static void nearpln(OBJECTS *ob)
 {
 	OBJECTS *obt, *obc;
-	int obx, obclr;
+	int obx;
 
 	obx = ob->ob_x;
-	obclr = ob->ob_owner->ob_clr;
 
 	for (obt = objtop; obt != NULL; obt = obt->ob_next) {
+		// TODO: Planes currently target any plane of a different
+		// faction. In the future it would be nice to support
+		// alliances between factions.
 		if (obt->ob_type != PLANE
-		 || obclr == obt->ob_owner->ob_clr) {
+		 || obt->ob_faction == ob->ob_faction) {
 			continue;
 		}
 
@@ -825,10 +827,16 @@ static OBJECTS *FindEnemyPlane(OBJECTS *ob)
 
 	// TODO: We can do better than scanning the entire object list.
 	for (obp = objtop; obp != NULL; obp = obp->ob_next) {
-		// Only shoot at enemy planes. In single player mode, computer
-		// planes do not get targeted.
-		if (obp->ob_type != PLANE || obp->ob_clr == ob->ob_clr ||
-		    (playmode != PLAYMODE_ASYNCH && obp != planes[0])) {
+		// TODO: Targets consider any plane of a different faction
+		// to be an enemy. In the future we may want to support
+		// alliances between factions.
+		if (obp->ob_type != PLANE
+		 || obp->ob_faction == ob->ob_faction) {
+			continue;
+		}
+		// In single player mode, computer planes do not get targeted.
+		if (playmode != PLAYMODE_ASYNCH
+		 && obp->ob_faction != FACTION_PLAYER1) {
 			continue;
 		}
 		if (PlaneIsKilled(obp->ob_state)) {

commit 4bd39085c0bdc224d5f50d100b3cc0e5d8728bbe

Fix crash during multiplayer games
This was also caused by commit 7f8393dc12bdf611; my refactor mistakenly
was using `planes[2]` for player 2, when player 2 is actually
`planes[1]`.

This fixes #55. Thanks to @alphanumericcharter and @scandox for
reporting this issue.

src/swcollsn.c
@@ -130,8 +130,7 @@ static OBJECTS *GetScoreObject(OBJECTS *ob, int *reverse)
 		        || ob->ob_faction == FACTION_NONE;
 	} else {
 		// TODO: Support more than two factions.
-		retval = planes[ob->ob_faction == FACTION_PLAYER1 ?
-		                FACTION_PLAYER2 : FACTION_PLAYER1];
+		retval = planes[ob->ob_faction == FACTION_PLAYER1 ? 1 : 0];
 	}
 
 	return retval;